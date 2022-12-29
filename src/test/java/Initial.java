842ad6083a72082607787785f009d390c14d7e29
HP <pranav.patil0596@gmail.com>
2022-12-29 11:30:55 +0530

    Intial Configuration

---
+++ src/test/java/Initial.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.Set;
+
+public class Initial {
+    public static void main(String[] args) throws InterruptedException {
+        String baseDir = System.getProperty("user.dir");
+        System.setProperty("webdriver.chrome.driver",baseDir + "\\src\\main\\resources\\drivers\\chromedriver.exe");
+        WebDriver driver =new ChromeDriver();
+        driver.get("https://www.calculator.net/percent-calculator.html");
+
+        Thread.sleep(2000);
+
+        driver.quit();
+
+
+
+
+    }
+
+}
